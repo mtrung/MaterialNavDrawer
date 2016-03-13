4478cb2ee882432ad623cb6151ca30d646d90ac2
Trung Vo <mtrung@gmail.com>
2016-03-13 05:10:31 -0500

    improve nav item

--- src/main/java/com/inoapp/lib/navdrawer/NavigationItem.java
+++ src/main/java/com/inoapp/lib/navdrawer/NavigationItem.java
@@ -1,8 +1,10 @@
 package com.inoapp.lib.navdrawer;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 
 /**
  * Created by tvo on 5/5/2015.
@@ -10,12 +12,26 @@ import android.graphics.drawable.Drawable;
 public class NavigationItem {
     private String mText;
     private Drawable mDrawable;
+    public Class mFragmentClass;
 
     public Fragment mFragment;
+
 
     public NavigationItem(String text, Drawable drawable) {
         mText = text;
         mDrawable = drawable;
+    }
+    public NavigationItem(String text, Drawable drawable, Class fragmentClass) {
+        mText = text;
+        mDrawable = drawable;
+        mFragmentClass = fragmentClass;
+    }
+
+    public static NavigationItem newInstance(Context context, int strResId, int drawableResId, Class fragmentClass) {
+        return new NavigationItem(
+                context.getString(strResId),
+                ContextCompat.getDrawable(context, drawableResId),
+                fragmentClass);
     }
 
     public String getText() {
@@ -32,5 +48,13 @@ public class NavigationItem {
 
     public void setDrawable(Drawable drawable) {
         mDrawable = drawable;
+    }
+
+    public Fragment createFragment() throws IllegalAccessException, InstantiationException {
+        if (mFragmentClass == null) return null;
+        if (mFragment != null) return null;
+
+        mFragment = (Fragment) mFragmentClass.newInstance();
+        return mFragment;
     }
 }
